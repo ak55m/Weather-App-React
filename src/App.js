45dc8c4c2448952c82438dd9ae3e3e0b676def6b
Akeem Mohammed <60115485+ak55m@users.noreply.github.com>
2022-05-11 01:34:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const weather_api = {
     key : "82736524bda615f358b15f9c9e2beb9b",
-    baseURL : "https://api.openweathermap.org/data/2.5/weather";
+    baseURL : "https://api.openweathermap.org/data/2.5/weather"
 }
 
 function App() {
